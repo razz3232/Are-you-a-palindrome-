3a781956b616e8b789a8b23ea89f2000de38cdfc
razz3232 <74662119+razz3232@users.noreply.github.com>
2024-02-05 16:28:25 +0000

    output done

--- Palindrome/Main.java
+++ Palindrome/Main.java
@@ -3,16 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "1";
-        String s2 = s1.concat("2");
-        s2.concat("3");
-        System.out.println(s2);
-        String name = "Marco";
-        System.out.println(name.length());
-        System.out.println(name.charAt(3));
-
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a Word ;");
+        System.out.println("Enter a Word :");
         String palin = input.nextLine();
         StringBuilder sb = new StringBuilder(palin);
         String message = (palin.equalsIgnoreCase(sb.reverse().toString()))?"Palindrome": "not Palindrome";
